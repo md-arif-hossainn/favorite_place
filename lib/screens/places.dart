a7e693de42e8536db72c030ede23cc0a7c539294
hossain_arif <arif134221@gmail.com>
2024-08-15 10:54:42 +0600

    Add two screen

---
+++ lib/screens/places.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/places_list.dart';
+import 'add_place.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: [],
+      ),
+    );
+  }
+}
